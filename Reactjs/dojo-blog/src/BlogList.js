6ccbaf9869b4001c74a4f3c4665b59c743af82cf
Williano <paawilly17@gmail.com>
2022-06-05 15:59:50 -0500

    Add props

---
+++ Reactjs/dojo-blog/src/BlogList.js
@@ -0,0 +1,29 @@
+const BlogList = ({ props }) => {
+  return (
+    <div className="blog-list">
+      {props.blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
+
+// const BlogList = ({ blogs }) => {
+//   return (
+//     <div className="blog-list">
+//       {blogs.map((blog) => (
+//         <div className="blog-preview" key={blog.id}>
+//           <h2>{blog.title}</h2>
+//           <p>Written by {blog.author}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default BlogList;
