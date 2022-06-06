96ba9bc23b143be934350ef5325acc485e6a371d
Williano <paawilly17@gmail.com>
2022-06-06 00:19:19 -0500

    Add article detail page

--- Reactjs/dojo-blog/src/BlogDetails.js
+++ Reactjs/dojo-blog/src/BlogDetails.js
@@ -1,11 +1,26 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8010/blogs/" + id);
 
   return (
     <div className="blog-details">
-      <h2>Blog details {id}</h2>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
