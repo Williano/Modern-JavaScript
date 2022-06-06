32543d5e872f8991e5f2293410fac9cc011ff3b1
Williano <paawilly17@gmail.com>
2022-06-05 21:07:24 -0500

    Add api request

--- Reactjs/dojo-blog/src/BlogList.js
+++ Reactjs/dojo-blog/src/BlogList.js
@@ -1,11 +1,10 @@
-const BlogList = ({ blogs, handleDelete }) => {
+const BlogList = ({ blogs }) => {
   return (
     <div className="blog-list">
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
         </div>
       ))}
     </div>
--- Reactjs/dojo-blog/src/Home.js
+++ Reactjs/dojo-blog/src/Home.js
@@ -2,30 +2,25 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-
-    setBlogs(newBlogs);
-  };
+  const [isLoading, setIsLoading] = useState(true);
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("use effect ran");
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsLoading(false);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} handleDelete={handleDelete} />
+      {isLoading ? <div>Loading....</div> : <BlogList blogs={blogs} />}
     </div>
   );
 };
